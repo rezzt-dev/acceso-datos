57550d655623da6c36ce032abffec7cf73828c06
juangc.zip <jgarciac111@educastillalamancha.es>
2024-10-14 11:04:50 +0200

    actualizacion 1.5

--- resultados-2/fileAccesoAleatorio/src/main/java/com/jgc/fileaccesoaleatorio/vista/InterfazVista.java
+++ resultados-2/fileAccesoAleatorio/src/main/java/com/jgc/fileaccesoaleatorio/vista/InterfazVista.java
@@ -13,7 +13,13 @@ package com.jgc.fileaccesoaleatorio.vista;
  */
 public interface InterfazVista {
  // atributos & constantes =>
+  static final String LEER_EMPLEADO = "lee un empleado";
+  static final String MOSTRAR_REGISTROS = "muestra los registros guardados";
   
+  static final String ESCRIBIR_EMPLEADO_FINAL_ARCHIVO = "escribe un empleado al final del archivo";
+  static final String MODIFICAR_APELLIDO_EMPLEADO = "modifica el apellido del empleado";
+  static final String ALMACENAR_REGISTRO = "guarda el registro en el archivo";
+  static final String BORRADO_LOGICO = "realiza un borrado logico de los registros";
   
  //——————————————————————————————————————————————————————————————————————
  // constructores =>
---
+++ resultados-2/fileAccesoAleatorio/src/main/java/com/jgc/fileaccesoaleatorio/vista/VentanaTexto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.jgc.fileaccesoaleatorio.vista;
+
+/**
+ *
+ * @author JGC by Juan Garcia Cazallas
+ * @version 1.0
+ * Created on 14 oct 2024
+ */
+public class VentanaTexto implements InterfazVista {
+  
+}
