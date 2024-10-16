1c9a79bb472e99468b286a20bca4b4def25363b2
juangc.zip <jgarciac111@educastillalamancha.es>
2024-10-16 12:52:54 +0200

    update 2.1 | fileDOMXml update

--- resultados-2/fileDOMXml/src/main/java/com/jgc/filedomxml/FileDOMXml.java
+++ resultados-2/fileDOMXml/src/main/java/com/jgc/filedomxml/FileDOMXml.java
@@ -5,13 +5,31 @@
 package com.jgc.filedomxml;
 
 import com.jgc.filedomxml.modelo.GestionContenidoDOM;
+//import org.w3c.dom.Element;
 
 /**
  *
  * @author JGC by Juan Garcia Cazallas
  */
 public class FileDOMXml {
-    public static void main(String[] args) {
-        GestionContenidoDOM modeloDOM = new GestionContenidoDOM("Empleados");
-    }
+  public static void main(String[] args) {
+    String generalName = "Empleados";
+    GestionContenidoDOM modeloDOM = new GestionContenidoDOM(generalName);
+    
+//    Element elem = modeloDOM.addNodo("Empleado");
+//    modeloDOM.addNodoTexto("identificador", "1", elem);
+//    
+//    Element elem2 = modeloDOM.addNodo("Empleado");
+//    modeloDOM.addNodoTexto("identificador", "2", elem2);
+//    
+//    Element elem3 = modeloDOM.addNodo("Empleado");
+//    modeloDOM.addNodoTexto("identificador", "3", elem3);
+
+    
+    
+    
+//    modeloDOM.generateFileFromDOM("./Resources/"+ generalName +".xml");
+    modeloDOM.chargeFileInMemory("./Resources/"+ generalName +".xml");
+    modeloDOM.screenPrinter();
+  }
 }
--- resultados-2/fileDOMXml/src/main/java/com/jgc/filedomxml/modelo/GestionContenidoDOM.java
+++ resultados-2/fileDOMXml/src/main/java/com/jgc/filedomxml/modelo/GestionContenidoDOM.java
@@ -5,13 +5,27 @@
 
 package com.jgc.filedomxml.modelo;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -30,6 +44,7 @@ public class GestionContenidoDOM {
   public GestionContenidoDOM (String inputNombre) {
     try {
       this.docFactory = DocumentBuilderFactory.newInstance();
+      docFactory.setIgnoringElementContentWhitespace(true);
       this.docBuilder = this.docFactory.newDocumentBuilder();
       
       DOMImplementation domImplementation = this.docBuilder.getDOMImplementation();
@@ -39,14 +54,83 @@ public class GestionContenidoDOM {
       Logger.getLogger(GestionContenidoDOM.class.getName()).log(Level.SEVERE, null, ex);
     }
     
-    System.out.println(" > documento creado en memoria.");
+    System.out.println(" > documento '" + inputNombre + "' creado en memoria.");
   }
   
  //——————————————————————————————————————————————————————————————————————
- // metodos privados =>
+  // metodos privados =>
+   // metodo "preProcess" | crea y devuelve un transformer ->
+  private Transformer preProcess () {
+    Transformer domTransformer = null;
+    
+    try {
+      domTransformer = TransformerFactory.newInstance().newTransformer();
+      domTransformer.setOutputProperty(OutputKeys.INDENT, "yes");
+      domTransformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2"); 
+    } catch (TransformerConfigurationException ex) {
+      Logger.getLogger(GestionContenidoDOM.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    
+    return domTransformer;
+  }
+  
  //——————————————————————————————————————————————————————————————————————
   // metodos publicos =>
+   // metodo "addNodo" | agrega un nodo principal al XML ->
+  public Element addNodo (String inputNombreNodo) {
+    Element mainNodo = this.documento.createElement(inputNombreNodo);
+    this.documento.getDocumentElement().appendChild(mainNodo);
+    return mainNodo;
+  }
+  
+   // metodo "addNodo" | agrega un nodo hijo a otro nodo ->
+  public Element addNodo (String inputDatoEmple, Element nodoPadre) {
+    Element dato = this.documento.createElement(inputDatoEmple);
+    nodoPadre.appendChild(dato);
+    
+    return dato;
+  }
+  
+   // metodo "addNodoTexto" | agrega a un nodo un valor ->
+  public void addNodoTexto (String inputDatoEmple, String inputTexto, Element nodoRoot) {
+    Element dato = this.documento.createElement(inputDatoEmple);
+    Text textoData = this.documento.createTextNode(inputTexto);
+    
+    dato.appendChild(textoData);
 
+    nodoRoot.appendChild(dato);
+  }
+  
+   // metodo "screenPrinter" | muestra en pantalla el resultado ->
+  public void screenPrinter () {
+    try {
+      Source domSource = new DOMSource(this.documento);
+      Result domResult = new StreamResult(System.out);
+      preProcess().transform(domSource, domResult);
+    } catch (TransformerException ex) {
+      Logger.getLogger(GestionContenidoDOM.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+  
+  public void generateFileFromDOM (String inputFilename) {
+    try {
+      Source domSource = new DOMSource(this.documento);
+      Result domResult = new StreamResult(new File(inputFilename));
+      preProcess().transform(domSource, domResult);
+    } catch (TransformerException ex) {
+      Logger.getLogger(GestionContenidoDOM.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+  
+  public void chargeFileInMemory (String inputFilename) {
+    try {
+      this.documento = this.docBuilder.parse(new File(inputFilename));
+      this.documento.getDocumentElement().normalize();
+      
+    } catch (SAXException | IOException ex) {
+      Logger.getLogger(GestionContenidoDOM.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
   
  //——————————————————————————————————————————————————————————————————————
  // getters & setters =>
