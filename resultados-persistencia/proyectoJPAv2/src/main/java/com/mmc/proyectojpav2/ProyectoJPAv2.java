87c637bff2d80c51d2862bc3854b66fc154105fb
juangc.zip <jgarciac111@educastillalamancha.es>
2024-12-04 13:25:42 +0100

    AAA

--- resultados-persistencia/proyectoJPAv2/src/main/java/com/mmc/proyectojpav2/ProyectoJPAv2.java
+++ resultados-persistencia/proyectoJPAv2/src/main/java/com/mmc/proyectojpav2/ProyectoJPAv2.java
@@ -6,8 +6,13 @@ package com.mmc.proyectojpav2;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.LockModeType;
 import jakarta.persistence.Persistence;
-import java.math.BigDecimal;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -47,7 +52,9 @@ public class ProyectoJPAv2 {
 //            Logger.getLogger(ProyectoJPAv2.class.getName()).log(Level.SEVERE, null, ex);
 //        }
 
-        leerRegistroDept();
+        leerRegistroRelacionadoEmpleDept();
+        modificarDatosDept( 50, "CONSTRUCCION", "VALENCIA");
+        modificarDatosDeptSeguro(10, "MODA", "LUGO");
         
         cerrarFactory();
         
@@ -71,5 +78,100 @@ public class ProyectoJPAv2 {
       } else {
         System.out.println(" > No existe el registro");
       }
+    }
+    
+    public static void leerRegistroBloqueDept () {
+      entitymanager.getTransaction().begin();
+      
+      departamentos = entitymanager.find(Departamentos.class, (short) 10, LockModeType.PESSIMISTIC_READ);
+      
+      if (departamentos != null) {
+        System.out.println(" > Dept NAME: " + departamentos.getDnombre());
+      } else {
+        System.out.println(" > No existe el registro");
+      }
+      
+      entitymanager.getTransaction().commit();
+    }
+    
+    public static void leerRegistroRelacionadoEmpleDept () {
+      Empleados tempEmple;
+      
+      departamentos = entitymanager.find(Departamentos.class, (short) 10);
+      
+      if (departamentos != null) {
+        System.out.println(" > Dept NAME: " + departamentos.getDnombre());
+        
+        Collection<Empleados> listEmple = departamentos.getEmpleadosCollection();
+        
+        Iterator<Empleados> it = listEmple.iterator();
+        
+        while (it.hasNext()) {
+          tempEmple = it.next();
+          System.out.println("  - Emple NAME: " + tempEmple.getApellido());
+        }
+      } else {
+        System.out.println(" > No existe el registro");
+      }
+    }
+    
+    public static void esperar () {
+      try {
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        
+        System.out.println(" > Pulsa ENTER para continuar...");
+        String sTexto = br.readLine();
+      } catch (IOException ex) {
+        Logger.getLogger(ProyectoJPAv2.class.getName()).log(Level.SEVERE, null, ex);
+      }
+    }
+    
+    public static void recargarDeptBBDD () {
+      entitymanager.getTransaction().begin();
+      entitymanager.refresh(departamentos);
+      entitymanager.getTransaction().commit();
+    }
+    
+    public static void insertarDatosDept (int inputDeptNo, String inputNombre, String inputLoc) {
+      Departamentos dept;
+      
+      dept = new Departamentos();
+      dept.setDeptNo((short) inputDeptNo);
+      dept.setDnombre(inputNombre);
+      dept.setLoc(inputLoc);
+      
+      entitymanager.getTransaction().begin();
+      entitymanager.persist(dept);
+      entitymanager.getTransaction().commit();
+    }
+    
+    public static void modificarDatosDept (int inputDeptNo, String newNombre, String newLoc) {
+      departamentos = entitymanager.find(Departamentos.class, (short) inputDeptNo);
+      
+      entitymanager.getTransaction().begin();
+      departamentos.setDnombre(newNombre);
+      departamentos.setLoc(newLoc);
+      entitymanager.getTransaction().commit();
+    }
+    
+    public static void modificarDatosDeptSeguro (int inputDeptNo, String newNombre, String newLoc) {
+      entitymanager.getTransaction().begin();
+      departamentos = entitymanager.find(Departamentos.class, (short) inputDeptNo, LockModeType.PESSIMISTIC_READ);
+      entitymanager.getTransaction().commit();
+      
+      entitymanager.getTransaction().begin();
+      departamentos.setDnombre(newNombre);
+      departamentos.setLoc(newLoc);
+      entitymanager.getTransaction().commit();
+    }
+    
+    public static void borrarDatosDept (int inputDeptNo) {
+      entitymanager.getTransaction().begin();
+      departamentos = entitymanager.find(Departamentos.class, (short) inputDeptNo, LockModeType.PESSIMISTIC_READ);
+      entitymanager.getTransaction().commit();
+      
+      entitymanager.getTransaction().begin();
+      entitymanager.remove(departamentos);
+      entitymanager.getTransaction().commit();
     }
 }
